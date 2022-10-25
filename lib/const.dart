01e8a10275ea8b8a70011fc2578cd5b9ff05dfed
arjunonair <arjunonair123@gmail.com>
2022-10-25 21:01:54 +0530

    init commit

---
+++ lib/const.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+
+const backgroundColor = Color.fromRGBO(45, 27, 3, 1);
+const frontgroundColor = Color.fromARGB(255, 222, 147, 50);
+
+
--- lib/start_quiz_screen.dart
+++ lib/start_quiz_screen.dart
@@ -1,10 +1,47 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:quiz_app/const.dart';
 
 class StartQuizScreen extends StatelessWidget {
   const StartQuizScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        title: const Text(
+          "Start quiz ",
+          style: TextStyle(color: backgroundColor),
+        ),
+        centerTitle: true,
+        backgroundColor: frontgroundColor,
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Quiz App",
+                style: TextStyle(
+                  color: frontgroundColor,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w500,
+                )),
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  primary: frontgroundColor,
+                ),
+                child: Text(
+                  "start quiz",
+                  style: TextStyle(color: backgroundColor),
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
